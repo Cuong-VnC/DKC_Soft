f5b1c957de4728c1451139bfef5dd202bda4ae9d
Cuong-VnC <cuongvunhat755@gmail.com>
2026-08-06 18:05:58 +0700

    Web migration: Bypassed licensing and restructured folders

--- temp_git_hf/app.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-from main import app
-
-if __name__ == "__main__":
-    # HuggingFace Spaces expects the app to run on host 0.0.0.0 and port 7860
-    uvicorn.run("main:app", host="0.0.0.0", port=7860)
